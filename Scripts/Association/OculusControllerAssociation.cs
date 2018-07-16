67f27d2ca1c95742151b5cbd1a597d6b5063dc0d
Christopher - Marcel Böddecker <bddckr@icloud.com>
2018-07-16 09:47:50 +0200

    feat(Tracking): determine controller type
    A new type has been added to allow turning on/off game objects based
    on whether a specific type of controller is currently connected or
    additionally the active controller.

---
+++ Scripts/Association/OculusControllerAssociation.cs
@@ -0,0 +1,30 @@
+﻿namespace VRTK.OculusUtilities.Association
+{
+    using UnityEngine;
+    using VRTK.Core.Association;
+
+    /// <summary>
+    /// Holds <see cref="GameObject"/>s to (de)activate based on a <see cref="OVRInput.Controller"/>.
+    /// </summary>
+    public class OculusControllerAssociation : BaseGameObjectsAssociation
+    {
+        /// <summary>
+        /// The <see cref="OVRInput.Controller"/> that needs to be connected.
+        /// </summary>
+        [Tooltip("The OVRInput.Controller that needs to be connected.")]
+        public OVRInput.Controller controller = OVRInput.Controller.None;
+
+        /// <summary>
+        /// Whether the <see cref="controller"/> needs to be active.
+        /// </summary>
+        [Tooltip("Whether the controller needs to be active.")]
+        public bool needsToBeActive;
+
+        /// <inheritdoc />
+        public override bool ShouldBeActive()
+        {
+            return OVRInput.IsControllerConnected(controller)
+                && ((OVRInput.GetActiveController() == controller) == needsToBeActive);
+        }
+    }
+}
